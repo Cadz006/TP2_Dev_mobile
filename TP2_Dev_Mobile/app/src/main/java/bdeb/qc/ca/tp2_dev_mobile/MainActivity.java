40df8c42926329e3cc12b5a515c98f4468d1b535
nphalakhone <nick.phalakhone@gmail.com>
2019-11-08 11:44:11 -0500

    Creation du login

--- TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/MainActivity.java
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/MainActivity.java
@@ -1,14 +1,23 @@
 package bdeb.qc.ca.tp2_dev_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
+        toolbarSetup();
+    }
+
+    private void toolbarSetup() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.toolbarTitle));
+        setSupportActionBar(toolbar);
     }
 }
