9a1281e8865dcac5d5e656dca3478e5e39e0ed3e
nphalakhone <nick.phalakhone@gmail.com>
2019-11-08 13:00:32 -0500

    Continuation du login

--- TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/MainActivity.java
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/MainActivity.java
@@ -3,8 +3,11 @@ package bdeb.qc.ca.tp2_dev_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        final EditText etxtEmail = findViewById(R.id.etxtEmail);
+        final EditText etxtPassword = findViewById(R.id.etxtPassword);
+
         toolbarSetup();
+        btnListener(etxtEmail, etxtPassword);
+    }
+
+    private void btnListener(final EditText etEmail, final EditText etPassword) {
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etEmail.getText().toString().trim().isEmpty()) {
+                    etEmail.setError("Veuillez entrer un émail!");
+                }
+                else if (etPassword.getText().toString().trim().isEmpty()) {
+                    etPassword.setError("Veuillez entrer un mot de passe!");
+                }
+            }
+        });
     }
 
     private void toolbarSetup() {
