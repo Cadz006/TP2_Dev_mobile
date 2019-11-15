bd17d0fb7001133e7fc1b6d13092857bbf137eb9
Gdelb <gui.dlb.37@gmail.com>
2019-11-15 11:40:46 -0500

    commentaire

---
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Commentaire.java
@@ -0,0 +1,14 @@
+package bdeb.qc.ca.tp2_dev_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Commentaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commentaire);
+    }
+}
