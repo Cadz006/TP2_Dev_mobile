354c2d15a9ab55b38e7587e79bce18c0ba4bcd02
Mathieu <mathieularivee00@gmail.com>
2019-11-17 10:19:25 -0500

    Created project files

---
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/MetierActivity.java
@@ -0,0 +1,16 @@
+package bdeb.qc.ca.tp2_dev_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetierActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metier);
+    }
+}
