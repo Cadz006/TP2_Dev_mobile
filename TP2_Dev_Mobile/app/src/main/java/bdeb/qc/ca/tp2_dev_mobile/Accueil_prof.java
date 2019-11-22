e1d89c90ccef923539d12b92d040400f4437cc40
RSP006 <o.cadieux006@hotmail.ca>
2019-11-22 12:09:25 -0500

    adapter etudiant est fini

--- TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Accueil_prof.java
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Accueil_prof.java
@@ -3,9 +3,11 @@ package bdeb.qc.ca.tp2_dev_mobile;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,7 +16,7 @@ import java.util.ArrayList;
 
 public class Accueil_prof extends AppCompatActivity {
 
-    private ArrayList<Etudiant> listClient;
+    private ArrayList<Etudiant> listEtudiant;
     private RecyclerView recyclerView;
     private EtudiantAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -24,14 +26,32 @@ public class Accueil_prof extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil_prof);
-        toolbarSetup();
+        setup();
+
+        adapter.setOnItemClickListener(new EtudiantAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                notifyItemSelected(position);
+            }
+        });
 
     }
 
-    private void toolbarSetup() {
+    private void setup() {
         Toolbar toolbar = findViewById(R.id.toolbarAcceuil);
         toolbar.setTitle(getResources().getString(R.string.toolBarAccProf));
         setSupportActionBar(toolbar);
+
+        listEtudiant = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new EtudiantAdapter(listEtudiant);
+    }
+
+    public void notifyItemSelected(int position) {
+        Log.v("RecyclerView", "" + position + " a été sélectionné: " + listEtudiant.get(position).getNom());
     }
 
     @Override
