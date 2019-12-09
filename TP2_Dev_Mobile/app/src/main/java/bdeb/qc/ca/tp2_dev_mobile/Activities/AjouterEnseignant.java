1d8cc1462fba4755bda0ab14cb88a2c42bd61a16
Gdelb <gui.dlb.37@gmail.com>
2019-12-09 17:27:16 -0500

    adminitrateur enseignant

--- TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Activities/AjouterEnseignant.java
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Activities/AjouterEnseignant.java
@@ -1,22 +1,23 @@
 package bdeb.qc.ca.tp2_dev_mobile.Activities;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import bdeb.qc.ca.tp2_dev_mobile.R;
 
 public class AjouterEnseignant extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accueil_admin);
+        setContentView(R.layout.activity_ajout_compte);
 
         setToolbar();
     }
---
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Adapters/AdminProfAdapter.java
@@ -0,0 +1,47 @@
+package bdeb.qc.ca.tp2_dev_mobile.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import bdeb.qc.ca.tp2_dev_mobile.R;
+
+public class AdminProfAdapter extends RecyclerView.Adapter<AdminProfAdapter.AdminProfViewHolder> {
+
+    private ArrayList<AdminProfViewHolder> rviList;
+
+    public static class AdminProfViewHolder extends RecyclerView.ViewHolder {
+        public AdminProfViewHolder (View v){
+            super(v);
+        }
+    }
+
+    public AdminProfAdapter (ArrayList<AdminProfViewHolder> rviList){
+        this.rviList =  rviList;
+    }
+
+    @NonNull
+    @Override
+    public AdminProfViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_ajout_compte,
+                parent, false);
+        AdminProfViewHolder ivh = new AdminProfViewHolder(v);
+        return ivh;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdminProfViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
