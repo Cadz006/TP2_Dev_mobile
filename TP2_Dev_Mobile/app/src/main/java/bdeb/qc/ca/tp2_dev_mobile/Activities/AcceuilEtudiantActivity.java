04f67a6113e1d42027c7a34463f5cab368c13163
Mathieu <mathieularivee00@gmail.com>
2019-12-09 18:26:22 -0500

    Comment + refactor AccueilEtudiantActivity

--- TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Activities/AcceuilEtudiantActivity.java
+++ TP2_Dev_Mobile/app/src/main/java/bdeb/qc/ca/tp2_dev_mobile/Activities/AcceuilEtudiantActivity.java
@@ -21,73 +21,106 @@ import bdeb.qc.ca.tp2_dev_mobile.R;
 public class AcceuilEtudiantActivity extends AppCompatActivity {
 
     private ArrayList<Metier> metierList;
-    private RecyclerView recyclerView;
     private MetierAdapter metierAdapter;
-    private RecyclerView.LayoutManager layoutManager;
     private boolean IsProf;
 
     public static final String KEY_METIER = "bdeb.qc.ca.tp2_dev_mobile.AcceuilEtudiantActivity.KEY_QUESTIONS";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acceuil_etudiant_activity);
+        setupToolbar();
+        addSampleDataToMetierList();
+        createRecyclerView();
+        addListenerToAdapter();
+        IsProf = getIntent().getBooleanExtra("IsProf", false);
+    }
 
-        toolbarSetup();
+    /**
+     * Cette méthode initialise les propriétés de la toolbar.
+     */
+    private void setupToolbar()
+    {
+        Toolbar toolbar = findViewById(R.id.toolbarAcceuil);
+        toolbar.setTitle(getResources().getString(R.string.toolbarTitle));
+        setSupportActionBar(toolbar);
+    }
 
+    /**
+     * Cette méthode ajoute des données dans la liste de métiers.
+     * Pour l'instant, elle sert parce que nous n'arrivons pas à avoir accès à l'API.
+     */
+    private void addSampleDataToMetierList()
+    {
         metierList = new ArrayList<>();
-
         metierList.add(new Metier("Machouin", 50));
         metierList.add(new Metier("Ewhatever", 70));
         metierList.add(new Metier("Torpinouche", 100));
         metierList.add(new Metier("Ipatente", 15));
         metierList.add(new Metier("Esti de BS", 0));
         metierList.add(new Metier("Rouyn-Noranda", 60));
+    }
 
-        recyclerView = findViewById(R.id.recViewMetierProg);
+    /**
+     * Cette méthode initialise les propriétés du recycler view et du adapter.
+     */
+    private void createRecyclerView()
+    {
+        RecyclerView recyclerView = findViewById(R.id.recViewMetierProg);
         recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         metierAdapter = new MetierAdapter(metierList);
-
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(metierAdapter);
+    }
 
-        metierAdapter.setOnItemClickListener(new MetierAdapter.OnItemClickListener() {
+    /**
+     * Cette méthode ajoute un événement au adapteur.
+     * L'événement ouvre une intent qui contient la liste des questions pour un métier spécifique.
+     */
+    private void addListenerToAdapter()
+    {
+        metierAdapter.setOnItemClickListener(new MetierAdapter.OnItemClickListener()
+        {
             @Override
-            public void onItemClick(int position) {
+            public void onItemClick(int position)
+            {
                 openActivityMetierLetter(position);
             }
         });
-
-        IsProf = getIntent().getBooleanExtra("IsProf", false);
     }
 
-    public void openActivityMetierLetter(int position) {
+    /**
+     * Cette méthode ouvre une intent qui contient la liste des questions pour un métier spécifique.
+     * @param position La position du métier dans la liste de métiers.
+     */
+    public void openActivityMetierLetter(int position)
+    {
         Intent intent = new Intent(this, QuestionListActivity.class);
         intent.putExtra(KEY_METIER, metierList.get(position));
-        if(IsProf) { intent.putExtra("IsProf", true); }
+        if (IsProf)
+        {
+            intent.putExtra("IsProf", true);
+        }
         startActivity(intent);
     }
 
-    private void toolbarSetup() {
-        Toolbar toolbar = findViewById(R.id.toolbarAcceuil);
-        toolbar.setTitle(getResources().getString(R.string.toolbarTitle));
-        setSupportActionBar(toolbar);
-    }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_acceuil, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.itemLogout:
-                finish();
-                break;
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == R.id.itemLogout)
+        {
+            finish();
         }
         return true;
     }
